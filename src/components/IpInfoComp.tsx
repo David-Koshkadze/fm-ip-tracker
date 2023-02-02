0774afb616bcc915ea29f507de93714271cb721c
m3taTron <rootUS3rr@pm.me>
2023-02-02 21:55:56 +0400

    add font

--- src/components/IpInfoComp.tsx
+++ src/components/IpInfoComp.tsx
@@ -4,15 +4,15 @@ function IpInfoComp({ ipAddress, location, timezone, isp }: IpDataTypes) {
   return (
     <div className="absolute left-0 right-0 mx-auto -bottom-32 md:-bottom-16 bg-white rounded-2xl flex flex-col md:flex-row justify-between items-center w-3/4 h-64 md:h-32 px-8 z-[9999] py-4 md:py-0">
       <div>
-        <span className="text-gray-400 text-xs font-bold uppercase">
+        <span className="text-gray-400 text-xs font-bold font-rubik uppercase">
           IP Address
         </span>
-        <p className="text-gray-900 text-lg font-bold">
+        <p className="text-gray-900 text-lg font-bold font-rubik">
           {ipAddress || "192.212.68.11"}
         </p>
       </div>
       <div>
-        <span className="text-gray-400 text-xs font-bold uppercase">
+        <span className="text-gray-400 text-xs font-bold font-rubik uppercase">
           Location
         </span>
         <p className="text-gray-900 text-lg font-bold">
@@ -20,13 +20,13 @@ function IpInfoComp({ ipAddress, location, timezone, isp }: IpDataTypes) {
         </p>
       </div>
       <div>
-        <span className="text-gray-400 text-xs font-bold uppercase">
+        <span className="text-gray-400 text-xs font-bold font-rubik uppercase">
           Timezone
         </span>
         <p className="text-gray-900 text-lg font-bold">{timezone || "UTC+4"}</p>
       </div>
       <div>
-        <span className="text-gray-400 text-xs font-bold uppercase">
+        <span className="text-gray-400 text-xs font-bold font-rubik uppercase">
           ISP
         </span>
         <p className="text-gray-900 text-lg font-bold">
